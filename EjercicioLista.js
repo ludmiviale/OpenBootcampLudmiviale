3d86199db49ebe5c328978ddbf0a275a84be9e8d
ludmiviale <111137199+ludmiviale@users.noreply.github.com>
2022-11-07 14:35:37 -0300

    Curso JavaScript
    Ejercicio Listas

---
+++ EjercicioLista.js
@@ -0,0 +1,10 @@
+const libro = {
+    titulo: "Mi planta de Naranja lima",
+    autor: "José Mauro de Vasconcelos",
+    año: 1968,
+    }
+
+const nacimiento = new Date("february 9 1994")
+
+const lista = ["Ludmila", 28, false, nacimiento, libro ]
+console.log(lista)
