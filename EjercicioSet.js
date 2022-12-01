973e027ee41f54a5b4dd19cbaf16199444ec65a0
ludmiviale <111137199+ludmiviale@users.noreply.github.com>
2022-12-01 17:01:59 -0300

    Ejercicio Set
    Curso JavaScript

---
+++ EjercicioSet.js
@@ -0,0 +1,10 @@
+const arrayFamilia = ["Susana", "Raúl", "Ruth", "Ludmila"]
+
+const setFamilia = new Set(arrayFamilia)
+console.log(setFamilia)
+
+setFamilia.add("Ludmila")
+console.log(setFamilia)
+
+setFamilia.add("JavaScript")
+console.log(setFamilia)
